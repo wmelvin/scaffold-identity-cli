d51d984b3b1a782a056570aba82700a03b993aba
Bill Melvin <bill@billmelvin.com>
2022-11-08 22:31:00 -0500

    Fill in UploadContext.

--- Data/UploadContext.cs
+++ Data/UploadContext.cs
@@ -2,19 +2,19 @@
 
 namespace demo_webapp.Data
 {
-    // public class UploadContext : DbContext
-    // {
-    //     public DbSet<Upload> Uploads => Set<Upload>();
+    public class UploadContext : DbContext
+    {
+        public DbSet<Upload> Uploads => Set<Upload>();
 
-    //     public string DbPath { get; set; }
+        public string DbPath { get; set; }
 
-    //     public UploadContext()
-    //     {
-    //         var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    //         DbPath = Path.Join(path, "demo-webapp.sqlite");
-    //     }
+        public UploadContext()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            DbPath = Path.Join(path, "demo-webapp.sqlite");
+        }
 
-    //     protected override void OnConfiguring(DbContextOptionsBuilder options)
-    //         => options.UseSqlite($"Data Source={DbPath}");
-    // }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
 }
