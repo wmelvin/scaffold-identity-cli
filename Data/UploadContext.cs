9908f81391c7d5d69360a7abd45921e26ff0b4dd
Bill Melvin <bill@billmelvin.com>
2022-11-08 22:25:49 -0500

    Add initial Upload data class.

---
+++ Data/UploadContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace demo_webapp.Data
+{
+    // public class UploadContext : DbContext
+    // {
+    //     public DbSet<Upload> Uploads => Set<Upload>();
+
+    //     public string DbPath { get; set; }
+
+    //     public UploadContext()
+    //     {
+    //         var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    //         DbPath = Path.Join(path, "demo-webapp.sqlite");
+    //     }
+
+    //     protected override void OnConfiguring(DbContextOptionsBuilder options)
+    //         => options.UseSqlite($"Data Source={DbPath}");
+    // }
+}
