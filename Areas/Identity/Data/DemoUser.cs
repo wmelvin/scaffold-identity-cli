20b16ecc2bc96ad1de59859988428bfc5f628116
Bill Melvin <bill@billmelvin.com>
2022-11-12 16:28:16 -0500

    After scaffold.

---
+++ Areas/Identity/Data/DemoUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace src.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the DemoUser class
+public class DemoUser : IdentityUser
+{
+}
+
